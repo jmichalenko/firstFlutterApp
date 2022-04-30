e7a491ad0ff41e29b7711c0fcec0bf0ebd63e09a
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2022-04-29 21:10:54 -0400

    Added Network Image as Background

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,29 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title:  Text('OUR LADY OF THE BAY'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'HELLO GRADE 11 ICS3U',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight:  FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower-Regular.ttf'
-        )
-        ),
-    ),  
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("Click"),
-      backgroundColor: Colors.red[600],
-      ),
-  ),
+  home: home(),
   
 ));
 
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+    appBar: AppBar(
+      title:  Text('OUR LADY OF THE BAY'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Image(
+        image: NetworkImage('https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNwYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'),
+        )       
+    ),  
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text("Click"),
+      backgroundColor: Colors.red[600],
+      ),
+  );
   }
 }
